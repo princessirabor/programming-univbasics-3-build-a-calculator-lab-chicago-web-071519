f0be380bf4c155987c44c66f913ca8fb69300f97
Princess Irabor <irabor.princess1@gmail.com>
2019-06-25 21:14:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,16 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+puts sum
 end
 
 def subtraction(num1, num2)
-
+difference = num1-num2
+puts difference
 end
 
 def division(num1, num2)
-
+quotient = num1/num2
+puts quotient
 end
 
 def multiplication(num1, num2)
