73e68a3d188a5d9da731b4f3aa9ed27dc14f764e
Princess Irabor <irabor.princess1@gmail.com>
2019-06-25 21:17:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,16 @@
 def addition(num1, num2)
 sum = num1 + num2
-puts sum
+sum
 end
 
 def subtraction(num1, num2)
 difference = num1-num2
-puts difference
+difference
 end
 
 def division(num1, num2)
 quotient = num1/num2
-puts quotient
+quotient
 end
 
 def multiplication(num1, num2)
@@ -25,5 +25,5 @@ end
 
 def square_root(num)
   sqroot = Math.sqrt(num)
-  sqrt
+  sqroot
 end
